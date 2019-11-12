0ad0993a0393bc3ffba6a307a0d499307716066b
lorena <lorenalies2312@gmail.com>
2019-11-12 14:09:24 -0500

    creating a server

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const express = require("express");
+const app = express();
 
+app.get("/", function(req, res) {
+  res.render("main.ejs");
+});
+
+app.listen(3004, function() {
+  console.log("my server is running better");
+});
