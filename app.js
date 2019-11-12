a318db22cc4a5d6bc4e3e83c99562848c2447bfc
lorena <lorenalies2312@gmail.com>
2019-11-12 15:43:36 -0500

    getting the app working

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+//start express app
 const app = express();
 
-app.get("/", function(req, res) {
-  res.render("main.ejs");
+// view engine
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// middeware
+app.use(express.static("./public"));
+
+let list = ["code and watch", "Slackline tonight"];
+
+//routs
+
+app.get("/home", function(req, res) {
+  res.render("main.ejs", { list: list });
+});
+
+app.post("/ninja", function(req, res) {
+  console.log(req.body.taskItem);
+  list.push(req.body.taskItem);
+  res.render("main.ejs", { list: list });
 });
 
 app.listen(3004, function() {
-  console.log("my server is running better");
+  console.log("my server is running now");
 });
