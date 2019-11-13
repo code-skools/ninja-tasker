dd1ec955ebee866674ff0792951a7bb3d82edd0e
uchennaofodile <uchenna.ofodile@gmail.com>
2019-11-13 13:28:22 -0500

    Added DOM and main.js

--- app.js
+++ app.js
@@ -6,17 +6,13 @@ const bodyParser = require("body-parser");
 // starting express app
 const app = express();
 
-// server listening for request
-app.listen(3000, function() {
-  console.log("API up and running");
-});
-
 // setting view engine
 app.set("view engine", "ejs");
 
 // middleware
 app.use(express.static("./public"));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 let list = ["Code and watch anime", "Slackline tonight"];
 
@@ -32,4 +28,18 @@ app.post("/ninja", function(req, res) {
   list.push(req.body.taskItem);
   res.render("home.ejs", { list: list });
   console.log(req);
+});
+
+//create a server to handle delete request
+app.delete("/delete/:index", function(req, res) {
+  console.log(req.params.index);
+
+  list.splice(req.params.index, 1);
+
+  res.json(list);
+});
+
+// server listening for request
+app.listen(3000, function() {
+  console.log("API up and running");
 });
